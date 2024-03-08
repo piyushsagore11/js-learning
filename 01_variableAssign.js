7845ae3f7abadf8c42d69b8112792bf12297fa6e
piyushsagore11 <piyushsagore11@gmail.com>
2024-03-09 02:21:54 +0530

    js_begining
    js_begining

---
+++ 01_variableAssign.js
@@ -0,0 +1,30 @@
+console.log("========Step 1========");
+
+var myFullName;
+console.log("my full name before initialisation is:",myFullName);
+myFullName="Piyush Sagore";
+console.log("After intialisation is:",myFullName);
+myFullName="Piyush Mohan Sagore";
+console.log("my full name after update is:",myFullName);
+
+console.log("========Step 2========");
+var myLove;
+myLove="I love only JS";
+console.log("value of my love is:",myLove);
+
+console.log("========Step 3========");
+
+var firstName="Piyush"
+console.log("firstName:",firstName);
+var lastName="Sagore"
+console.log("lastName:",lastName);
+var age="27";
+console.log("age:",age);
+var address="pune";
+console.log("address:",address);
+var pinCode="411033";
+console.log("pinCode:",pinCode);
+var gender="Male";
+console.log("gender:",gender);
+var IsMarried= "single";
+console.log("IsMarried:",IsMarried);
