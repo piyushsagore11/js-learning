f6cdd0f6bf70b381a1a5b2c1a09141f8cfaa74a0
piyushsagore11 <piyushsagore11@gmail.com>
2024-03-09 02:21:19 +0530

    assignments variable and swapping
    assignments variable and swapping

---
+++ 01_swapvariablesAssig.js
@@ -0,0 +1,21 @@
+console.log("========Step 1========");
+var sweety="sweety";
+console.log("sweety:",sweety);
+var cutie="cutie";
+console.log("cutie:",cutie);
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("sweety:", sweety, " cutie:", cutie);
+
+console.log("========Step 2========");
+var num1 = 100; 
+var num2 = 200;
+var num3 = 300;
+console.log("number before swaping:","num1:",num1 ,"num2:",num2, "num3:",num3 );
+console.log("after swap");
+var temp1=num1
+num1=num2;
+num2=num3;
+num3=temp1;
+console.log("number After swaping:","num1:",num1 ,"num2:",num2, "num3:",num3 );
