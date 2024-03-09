b6ab86251fd711fdf724b86db38096a296e3736f
piyushsagore11 <piyushsagore11@gmail.com>
2024-03-10 02:15:37 +0530

    Assignment of typeof datatype
    Assignment of typeof datatype

---
+++ 02_typeOf.js
@@ -0,0 +1,33 @@
+console.log("=========step 1=========");
+var bankName;
+console.log("Before inisilisation of variable for bank name:", bankName);
+bankName="SBI bank";
+console.log("After inisilisation of variable for bank name:", bankName);
+var typeofbank=typeof bankName;
+console.log("type of bank:", typeofbank);
+console.log("=========step 2=========");
+var marks="90%";
+console.log("marks obtained:", marks);
+var typeofmarks=typeof marks;
+console.log("data type of marks:", typeofmarks);
+
+console.log("=========step 3=========");
+var isWorking;
+console.log("Before inisilisation of variable for isWorking:", isWorking);
+var isWorking=true;
+console.log("After inisilisation of variable for isWorking:", isWorking);
+var typeofisWorking=typeof isWorking;
+console.log("type of value of isworking:",typeofisWorking);
+
+console.log("=========step 4=========");
+var totalCount="Hundred and Seven";
+console.log("value of total count is:",totalCount);
+var typeoftotalcount=typeof totalCount;
+console.log("datatype of total count is:",typeoftotalcount);
+totalCount=107;
+console.log("updated value of total count is:",totalCount);
+var typeoftotalcount=typeof totalCount;
+console.log("updated datatype of total count is:",typeoftotalcount);
+
+
+
