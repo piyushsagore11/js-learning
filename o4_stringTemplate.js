d6938963a29cb75461da4e67bef0b79bb6a196a9
piyushsagore11 <piyushsagore11@gmail.com>
2024-03-13 02:48:56 +0530

    string fnction template
    string fnction template

---
+++ o4_stringTemplate.js
@@ -0,0 +1,21 @@
+console.log(`I am "Software" Developer`);
+
+var city = "Pune";
+var firstName = "Gajanan";
+
+//console.log("My name is: ", firstName, " and city is: ", city);
+
+console.log(` My name is: ${firstName} and city is: ${city} `);
+console.log("========== replace() ==================");
+var greet = "Good Morning";
+const afterReplace = greet.replace("Morning", "Afternoon");
+console.log(`After replace string is: ${afterReplace}`);
+console.log(`========== toUpperCase() ==================`);
+var greet = "Good Morning";
+var greetInUpperCase = greet.toUpperCase();
+console.log(`${greet} in upper case is : ${greetInUpperCase}`);
+
+var str="My Name is CodingPanda.";
+console.log ('str.includes{name}');
+
+
