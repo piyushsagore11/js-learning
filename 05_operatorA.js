9df71c5f5b4d25e56d378aa4c2e61dbdb73de7c5
piyushsagore11 <piyushsagore11@gmail.com>
2024-03-14 22:48:57 +0530

    Assignment on string function and arithmetic operators
    Assignment on string function and arithmetic operators

---
+++ 05_operatorA.js
@@ -0,0 +1,23 @@
+ console.log(`======step 1================`);
+ 
+ function squareOfWorldLength(sentence){
+    var lengthOfSentence = sentence.length;
+    var squareLength =  lengthOfSentence**2;
+console.log(`length of the word ${sentence}:${squareLength}`);
+}
+squareOfWorldLength("JavaScript");
+squareOfWorldLength("Google chrome");
+squareOfWorldLength("Developer Smart");
+console.log(`======step 2================`);
+
+var statement="I am Angular Developer";
+var lengthOfStatement=statement.length;
+
+var noOfWords=statement.split(" ").length;
+var stringDivide=lengthOfStatement/noOfWords;
+console.log(`length of string after dividing is:${stringDivide}`);
+var stringmultiply=lengthOfStatement*noOfWords;
+console.log(`length of string after Multiply is:${stringmultiply}`);
+
+
+
