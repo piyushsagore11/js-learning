ac46ea312d35e07c9f1fe6078ac2c5b16fdfec7d
piyushsagore11 <piyushsagore11@gmail.com>
2024-03-19 17:12:41 +0530

    06_ForLoop_Assignment.js
    06_ForLoop_Assignment.js date 19-03-24

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,43 @@
+console.log(`//1. WAP to print numbers from 5 to 15 by incrementing 1  `);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+console.log(`//2. WAP to print numbers from 50 to 40. by decrementing 1  `);
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+console.log(`//3. WAP to find first 15 odd Numbers`);
+
+for (let index = 1, count=0 ; count<15; index=index+2 ,count++) {
+    console.log(index);
+    
+}
+console.log(`//4. WAP to find first 10 Even Numbers`);
+for (let index = 0, count=0; count<10; index=index+2,count++) {
+    console.log(index);
+    
+}
+console.log(`//5. WAP to print table of 5 like-> 5 10 15 .......50`);
+for (let index = 5, count=0; count<10; index=index+5 ,count++) {
+console.log(index);
+}
+
+
+    
+
+console.log(`//6. WAP to print table of 10 like->  10 20 30 40  .......100`);
+for (let index = 10; index <=100; index=index+10) {
+   
+    console.log(index);
+}
+
+console.log(`//7. WAP to print table of 10 like-> 100 90 80 70 ......10`);
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index);
+    
+}
+console.log(`different  method to find table`);
+for (let index = 0; index <=50; index=index+5) {
+    console.log(index);}
